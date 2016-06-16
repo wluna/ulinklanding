4bf59a9e1a79dedd883e296220fe4d4e0fb72d3c
Will Luna <william.luna@tufts.edu>
2016-06-16 15:16:43 -0300

    initial implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery 
+//= require materialize-sprockets 
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
